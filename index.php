eebb857c7ecaff2f8ee1aafaf869cb7cd5246563
lauramanerasanso <lauraams99@gmail.com>
2020-11-26 17:37:03 +0100

    trad bd

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
                 die("Connection failed: " . $conn->connect_error);
             }
 
-            $sql = "SELECT id, nom, preu FROM productes";
+            $sql = "SELECT productes.id as id, prod_lang.trad_nom as nom, productes.preu as preu from prod_lang join productes ON productes.id = prod_lang.id_prod where prod_lang.idioma=".$_SESSION['lang'].";";
             $result = $conn->query($sql);
 
             if ($result->num_rows > 0) {
