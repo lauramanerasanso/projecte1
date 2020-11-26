42c822479420f94862139065f2c872b2b2861ded
lauramanerasanso <lauraams99@gmail.com>
2020-11-26 15:42:56 +0100

    seguim

--- php/idioma.php
+++ php/idioma.php
@@ -4,12 +4,13 @@ session_start();
 if(isset($_SESSION['lang'])){
     if(isset($_GET['lang'])){
         $_SESSION['lang'] = $_GET['lang'];
-        header('Location: '.$_SERVER['REQUEST_URI']);
+        
     }
 }else{
     $_SESSION['lang']='ca';
-    header('Location: '.$_SERVER['REQUEST_URI']);
+    
 }
 
+header('Location: '.$_SERVER['PHP_SELF']);
 
 ?>
