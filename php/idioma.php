979f6b2f5ce7a1ed301ab1492a478eae772f683e
lauramanerasanso <lauraams99@gmail.com>
2020-11-26 15:54:16 +0100

    prova 1937472

--- php/idioma.php
+++ php/idioma.php
@@ -4,13 +4,10 @@ session_start();
 if(isset($_SESSION['lang'])){
     if(isset($_GET['lang'])){
         $_SESSION['lang'] = $_GET['lang'];
-        
     }
 }else{
     $_SESSION['lang']='ca';
-    
 }
 
-header('Location: '.$_SERVER['REQUEST_URI']);
-
+header('Location: '.$_SERVER['HTTP_REFERER']);
 ?>
