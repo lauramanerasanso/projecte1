125d6f88f68e1be77c7d888347f5287a344c4e12
lauramanerasanso <lauraams99@gmail.com>
2020-11-26 15:41:11 +0100

    pr

--- php/idioma.php
+++ php/idioma.php
@@ -4,10 +4,12 @@ session_start();
 if(isset($_SESSION['lang'])){
     if(isset($_GET['lang'])){
         $_SESSION['lang'] = $_GET['lang'];
+        header('Location: '.$_SERVER['REQUEST_URI']);
     }
 }else{
     $_SESSION['lang']='ca';
+    header('Location: '.$_SERVER['REQUEST_URI']);
 }
 
-header('Location: '.$_SERVER['REQUEST_URI']);
+
 ?>
